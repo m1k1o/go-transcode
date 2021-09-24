40942e30c278bb95fba8d09dfc7ce89281ba8340
klahaha <a@kl.netlib.re>
2021-09-24 16:16:20 +0200

    quick test for settings cascade

---
+++ test_args.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+# Number of seconds to wait for HTTP server to start
+# Test fails after this timeout
+TIMEOUT=1
+
+go build
+
+# Test default settings (:8080)
+output="$(timeout --preserve-status $TIMEOUT ./go-transcode serve 2>&1 3>&1)"
+if echo "$output" | grep ":8080" > /dev/null; then
+	echo "Default settings work"
+else
+	echo "Default settings failed:"
+	echo "$output"
+fi
+
+# Test env settings (:8889)
+output="$(TRANSCODE_BIND=":8889" timeout --preserve-status $TIMEOUT ./go-transcode serve 2>&1)"
+if echo "$output" | grep ":8889" > /dev/null; then
+	echo "Env settings work"
+else
+	echo "Env settings failed:"
+	echo "$output"
+fi
+
+# Test CLI settings (:8890)
+# We also check that CLI settings have higher priority than Env
+output="$(TRANSCODE_BIND=":8889" timeout --preserve-status $TIMEOUT ./go-transcode serve --bind ":8890" 2>&1)"
+if echo "$output" | grep ":8890" > /dev/null; then
+	echo "CLI settings work"
+else
+	echo "CLI settings failed:"
+	echo "$output"
+fi
+
+
