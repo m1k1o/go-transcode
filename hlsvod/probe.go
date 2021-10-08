3281cac25b0a1fa50bc3b6f4bd01e7212f988687
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-10-08 21:59:20 +0200

    add probe media.

--- hlsvod/probe.go
+++ hlsvod/probe.go
@@ -4,11 +4,116 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 )
+
+type ProbeMediaData struct {
+	FormatName []string
+	Duration   time.Duration
+
+	Video *ProbeVideoData
+	Audio []ProbeAudioData
+}
+
+func ProbeMedia(ctx context.Context, ffprobeBinary string, inputFilePath string) (*ProbeMediaData, error) {
+	args := []string{
+		"-v", "error", // Hide debug information
+		"-ignore_chapters", "1",
+		"-show_format",  // Show container information
+		"-show_streams", // Show codec information
+		"-of", "json",
+		inputFilePath,
+	}
+
+	cmd := exec.CommandContext(ctx, ffprobeBinary, args...)
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		// TODO: Handle stderr output.
+		log.Println(stderr.String())
+
+		return nil, err
+	}
+
+	out := struct {
+		Streams []struct {
+			CodecName string `json:"codec_name"`
+			CodecType string `json:"codec_type"`
+			Duration  string `json:"duration"`
+
+			// For video streams.
+			Width  int `json:"width"`
+			Height int `json:"height"`
+
+			// For audio streams.
+			BitRate string `json:"bit_rate"`
+		} `json:"streams"`
+		Format struct {
+			FormatName string `json:"format_name"`
+			Duration   string `json:"duration"`
+		} `json:"format"`
+	}{}
+
+	if err := json.Unmarshal(stdout.Bytes(), &out); err != nil {
+		return nil, err
+	}
+
+	data := ProbeMediaData{}
+	for _, stream := range out.Streams {
+		var duration time.Duration
+		if stream.Duration != "" {
+			duration, err = time.ParseDuration(stream.Duration + "s")
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse stream duration: %v", err)
+			}
+		}
+
+		switch stream.CodecType {
+		case "video":
+			if data.Video != nil {
+				log.Printf("found multiple video streams for %s\n", inputFilePath)
+			}
+
+			data.Video = &ProbeVideoData{
+				Width:    stream.Width,
+				Height:   stream.Height,
+				Duration: duration,
+			}
+		case "audio":
+			bitRate, err := strconv.ParseFloat(out.Streams[0].BitRate, 64)
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse audio stream bitrate: %v", err)
+			}
+
+			data.Audio = append(data.Audio, ProbeAudioData{
+				BitRate:  bitRate,
+				Duration: duration,
+			})
+		}
+	}
+
+	if out.Format.FormatName != "" {
+		data.FormatName = strings.Split(out.Format.FormatName, ",")
+	}
+
+	if out.Format.Duration != "" {
+		data.Duration, err = time.ParseDuration(out.Format.Duration + "s")
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse format duration: %v", err)
+		}
+	}
+
+	return &data, nil
+}
 
 type ProbeVideoData struct {
 	Width      int
