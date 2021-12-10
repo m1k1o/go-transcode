fcb5315867dd57f9d92dbcf7133800af6e135c61
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-12-10 18:39:06 +0100

    update main.go.

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/rs/zerolog/log"
-
 	"github.com/m1k1o/go-transcode/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		log.Panic().Err(err).Msg("failed to execute command")
-	}
+	_ = cmd.Execute()
 }
