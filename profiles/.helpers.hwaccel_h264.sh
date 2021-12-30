386a13695252f766d5b7010c3fe27644c2d9c9fb
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-12-30 13:11:47 +0100

    move hwaccel to a single helper file.

---
+++ profiles/.helpers.hwaccel_h264.sh
@@ -0,0 +1,112 @@
+
+INIT_HW_DEVICE="$(ffmpeg -init_hw_device list 2> /dev/null)"
+
+#
+# CUDA
+#
+CUDA_SUPPORTED="yes"
+
+if echo "$INIT_HW_DEVICE" | grep "cuda" > /dev/null; then
+    echo "[OK] CUDA is supported by ffmpeg."
+else
+    echo "[ERR] CUDA is not supported by ffmpeg."
+    CUDA_SUPPORTED="no"
+fi
+
+function cuvid_codec_to_encoder {
+    SUPPORTED_CODECS="h264 hevc mjpeg mpeg1video mpeg2video mpeg4 vc1 vp8 vp9"
+    CODEC=$(ffprobe -hide_banner -loglevel panic -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$1" | head -1)
+
+    if [[ ! " ${SUPPORTED_CODECS} " =~ " ${CODEC} " ]]; then
+        echo "${CODEC}"
+        return 1
+    fi
+
+    if [[ "${CODEC}" == "mpeg1video" ]]; then
+        echo "mpeg1_cuvid"
+        return 0
+    fi
+
+    if [[ "${CODEC}" == "mpeg2video" ]]; then
+        echo "mpeg2_cuvid"
+        return 0
+    fi
+
+    echo "${CODEC}_cuvid"
+}
+
+#
+# VAAPI
+#
+VAAPI_SUPPORTED="yes"
+
+if echo "$INIT_HW_DEVICE" | grep "vaapi" > /dev/null; then
+    echo "[OK] VAAPI is supported by ffmpeg."
+else
+    echo "[ERR] VAAPI is not supported by ffmpeg."
+    VAAPI_SUPPORTED="no"
+fi
+
+if vainfo 2>&1 > /dev/null; then
+    echo "[OK] vainfo command executed succesfully."
+else
+    echo "[ERR] vainfo command not found."
+    VAAPI_SUPPORTED="no"
+fi
+
+DRI_RENDER="/dev/dri/renderD128"
+if [ -e $DRI_RENDER ]; then
+    echo [OK] "$DRI_RENDER exists."
+else
+    echo "[ERR] $DRI_RENDER not found."
+    VAAPI_SUPPORTED="no"
+fi
+
+function vaapi_codec_to_encoder {
+    SUPPORTED_CODECS=$(vainfo 2>&1 | grep VAProfile | sed s/VAProfile// | sed "s/\(Simple\|Main\|High\|Advanced\|Baseline\|Scc\|Constrain\|Profile\).*//"| sed s/None.*// | sort | uniq | sed 's/\ //g')
+    CODEC=$(ffprobe -hide_banner -loglevel panic -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$1" | head -1)
+
+    if [[ ! " ${SUPPORTED_CODECS} " =~ " ${CODEC} " ]]; then
+        echo "${CODEC}"
+        return 1
+    fi
+
+    echo "${CODEC}_vaapi"
+}
+
+#
+# FFMPEG PARAMS
+#
+
+if [ "$CUDA_SUPPORTED" = "yes" ]; then
+    AVAILABLE_ENCODERS="$(ffmpeg -hide_banner -encoders | grep _cuvid | cut -d ' ' -f3)"
+
+    ENCODER="$(cuvid_codec_to_encoder "$INPUT")"
+    if [ "$?" = "1" ]; then
+        echo "[ERR] Unsupported codec: ${ENCODER:-???}"
+    elif [[ ! " ${AVAILABLE_ENCODERS} " =~ " ${ENCODER} " ]]; then
+        echo "[ERR] Unsupported encoder: ${ENCODER:-???}"
+    else
+        echo "Using CUDA hardware, codec $ENCODER."
+        export EXTRAPARAMS="-hwaccel_output_format cuda -c:v $ENCODER"
+        # TODO: Why no force_original_aspect_ratio here?
+        export VF="hwupload_cuda,yadif_cuda=0:-1:0,scale_npp=$VW:$VH:interp_algo=super"
+        export CV="h264_nvenc"
+    fi
+elif [ "$VAAPI_SUPPORTED" = "yes" ]; then
+    AVAILABLE_ENCODERS="$(ffmpeg -hide_banner -encoders | grep _vaapi | cut -d ' ' -f3)"
+
+    ENCODER="$(vaapi_codec_to_encoder "$INPUT")"
+    if [ "$?" = "1" ]; then
+        echo "[ERR] Unsupported codec: ${ENCODER:-???}"
+    elif [[ ! " ${AVAILABLE_ENCODERS} " =~ " ${ENCODER} " ]]; then
+        echo "[ERR] Unsupported encoder: ${ENCODER:-???}"
+    else
+        echo "Using using VAAPI hardware, codec $ENCODER."
+        export EXTRAPARAMS="-hwaccel vaapi -hwaccel_device $DRI_RENDER -hwaccel_output_format vaapi -c:v $ENCODER"
+        export VF="scale_vaapi=w=$VW:h=$VH:force_original_aspect_ratio=decrease"
+        export CV="h264_vaapi"
+    fi
+else
+    echo "No hardware acceleration available."
+fi
--- profiles/http_h264.sh
+++ profiles/http_h264.sh
@@ -1,44 +1,25 @@
 #!/usr/bin/env bash
 
+export INPUT="$1"
+
 if [[ "$VW" = "" ]]; then echo "Missing \$VW"; exit 1; fi
 if [[ "$VH" = "" ]]; then echo "Missing \$VH"; exit 1; fi
 if [[ "$ABANDWIDTH" = "" ]]; then echo "Missing \$ABANDWIDTH"; exit 1; fi
 if [[ "$VBANDWIDTH" = "" ]]; then echo "Missing \$VBANDWIDTH"; exit 1; fi
 if [[ "$VMAXRATE" = "" ]]; then echo "Missing \$VMAXRATE"; exit 1; fi
 if [[ "$VBUFSIZE" = "" ]]; then echo "Missing \$VBUFSIZE"; exit 1; fi
-#run vainfo to get exit code as output saved using $?
-vainfo 2>&1 > /dev/null
-#store exit code as support 0 means vaapi works 1 means software if cuda not detected
-VAAPISUPPORT=$(echo $?)
-CUDASUPPORT="$(ffmpeg -init_hw_device list 2> /dev/null)"
 
-if echo $CUDASUPPORT | grep "cuda" > /dev/null; then
-        echo "Using CUDA hardware."
-        source "$(dirname "$0")/.helpers.cuda.sh"
-        INPUT="$(cuvid_codec "${1}")"
-        # ffmpeg parameters
-        EXTRAPARAMS="-hwaccel_output_format cuda -c:v "$INPUT""
-        # TODO: Why no force_original_aspect_ratio here?
-        VF="hwupload_cuda,yadif_cuda=0:-1:0,scale_npp=$VW:$VH:interp_algo=super"
-        CV="h264_nvenc"
-fi
-if [ "$VAAPISUPPORT" -eq "0" ]; then
-        # TODO: vaapi support
-        source "$(dirname "$0")/.helpers.vaapi.sh"
-        #EXTRAPRAMAS="-hwaccel_output_format vaapi"
-        HWDECODE="-hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi"
-        echo "Using using VAAPI hardware (CPU fallback)."
-        VF="scale_vaapi=w=$VW:h=$VH:force_original_aspect_ratio=decrease"
-        CV="h264_vaapi"
-else
-        echo "Using CPU hardware."
-        VF="scale=w=$VW:h=$VH:force_original_aspect_ratio=decrease"
-        CV="h264"
+source "$(dirname "$0")/.helpers.hwaccel_h264.sh"
+
+if [ -z "$CV" ] || [ -z "$VF" ]; then
+  echo "Using CPU encoding."
+
+  VF="scale=w=$VW:h=$VH:force_original_aspect_ratio=decrease"
+  CV="h264"
 fi
 
 exec ffmpeg -hide_banner -loglevel warning \
-   $HWDECODE \
-  -i "${1}" $EXTRAPARAMS \
+  -i "$INPUT" $EXTRAPARAMS \
   -vf $VF \
     -c:a aac \
       -ar 48000 \
