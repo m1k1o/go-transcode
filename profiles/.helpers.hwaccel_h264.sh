dc7b4504e1bf531d1cdf96eb87135ad06ef7572d
noideaman <39884745+noideaman@users.noreply.github.com>
2021-12-30 14:41:27 -0700

    Detect only encode capablities for now

--- profiles/.helpers.hwaccel_h264.sh
+++ profiles/.helpers.hwaccel_h264.sh
@@ -63,7 +63,7 @@ else
 fi
 
 function vaapi_codec_to_encoder {
-    SUPPORTED_CODECS=$(vainfo 2>&1 | grep VAProfile | sed s/VAProfile// | sed "s/\(Simple\|Main\|High\|Advanced\|Baseline\|Scc\|Constrain\|Profile\).*//"| sed s/None.*// | sort | uniq | sed 's/\ //g')
+    SUPPORTED_CODECS=$(vainfo 2>&1 | grep VAProfile | sed s/VAProfile// | grep Enc | sed "s/\(Simple\|Main\|High\|Advanced\|Baseline\|Scc\|Constrain\|Profile\).*//"| sed s/None.*// | sort | uniq | sed 's/\ //g')
     CODEC=$(ffprobe -hide_banner -loglevel panic -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "$1" | head -1)
 
     if [[ ! " ${SUPPORTED_CODECS} " =~ " ${CODEC} " ]]; then
