3b155c4913abe639cb189b6a429e85d35e9312cb
Miroslav Šedivý <sedivy.miro@gmail.com>
2021-12-30 22:32:33 +0100

    fix error reporting hw helpers.

--- profiles/.helpers.hwaccel_h264.sh
+++ profiles/.helpers.hwaccel_h264.sh
@@ -7,9 +7,9 @@ INIT_HW_DEVICE="$(ffmpeg -init_hw_device list 2> /dev/null)"
 CUDA_SUPPORTED="yes"
 
 if echo "$INIT_HW_DEVICE" | grep "cuda" > /dev/null; then
-    echo "[OK] CUDA is supported by ffmpeg."
+    echo "[OK] CUDA is supported by ffmpeg." >&2
 else
-    echo "[ERR] CUDA is not supported by ffmpeg."
+    echo "[ERR] CUDA is not supported by ffmpeg." >&2
     CUDA_SUPPORTED="no"
 fi
 
@@ -41,24 +41,24 @@ function cuvid_codec_to_encoder {
 VAAPI_SUPPORTED="yes"
 
 if echo "$INIT_HW_DEVICE" | grep "vaapi" > /dev/null; then
-    echo "[OK] VAAPI is supported by ffmpeg."
+    echo "[OK] VAAPI is supported by ffmpeg." >&2
 else
-    echo "[ERR] VAAPI is not supported by ffmpeg."
+    echo "[ERR] VAAPI is not supported by ffmpeg." >&2
     VAAPI_SUPPORTED="no"
 fi
 
 if vainfo 2>&1 > /dev/null; then
-    echo "[OK] vainfo command executed succesfully."
+    echo "[OK] vainfo command executed succesfully." >&2
 else
-    echo "[ERR] vainfo command not found."
+    echo "[ERR] vainfo command not found." >&2
     VAAPI_SUPPORTED="no"
 fi
 
 DRI_RENDER="/dev/dri/renderD128"
 if [ -e $DRI_RENDER ]; then
-    echo [OK] "$DRI_RENDER exists."
+    echo "[OK] $DRI_RENDER exists." >&2
 else
-    echo "[ERR] $DRI_RENDER not found."
+    echo "[ERR] $DRI_RENDER not found." >&2
     VAAPI_SUPPORTED="no"
 fi
 
@@ -83,11 +83,11 @@ if [ "$CUDA_SUPPORTED" = "yes" ]; then
 
     ENCODER="$(cuvid_codec_to_encoder "$INPUT")"
     if [ "$?" = "1" ]; then
-        echo "[ERR] Unsupported codec: ${ENCODER:-???}"
+        echo "[ERR] CUDA - Unsupported codec: ${ENCODER:-???}" >&2
     elif [[ ! " ${AVAILABLE_ENCODERS} " =~ " ${ENCODER} " ]]; then
-        echo "[ERR] Unsupported encoder: ${ENCODER:-???}"
+        echo "[ERR] CUDA - Unsupported encoder: ${ENCODER:-???}" >&2
     else
-        echo "Using CUDA hardware, codec $ENCODER."
+        echo "Using CUDA hardware, codec $ENCODER." >&2
         export EXTRAPARAMS="-hwaccel_output_format cuda -c:v $ENCODER"
         # TODO: Why no force_original_aspect_ratio here?
         export VF="hwupload_cuda,yadif_cuda=0:-1:0,scale_npp=$VW:$VH:interp_algo=super"
@@ -98,15 +98,15 @@ elif [ "$VAAPI_SUPPORTED" = "yes" ]; then
 
     ENCODER="$(vaapi_codec_to_encoder "$INPUT")"
     if [ "$?" = "1" ]; then
-        echo "[ERR] Unsupported codec: ${ENCODER:-???}"
+        echo "[ERR] VAAPI - Unsupported codec: ${ENCODER:-???}" >&2
     elif [[ ! " ${AVAILABLE_ENCODERS} " =~ " ${ENCODER} " ]]; then
-        echo "[ERR] Unsupported encoder: ${ENCODER:-???}"
+        echo "[ERR] VAAPI - Unsupported encoder: ${ENCODER:-???}" >&2
     else
-        echo "Using using VAAPI hardware, codec $ENCODER."
+        echo "Using using VAAPI hardware, codec $ENCODER." >&2
         export EXTRAPARAMS="-hwaccel vaapi -hwaccel_device $DRI_RENDER -hwaccel_output_format vaapi -c:v $ENCODER"
         export VF="scale_vaapi=w=$VW:h=$VH:force_original_aspect_ratio=decrease"
         export CV="h264_vaapi"
     fi
 else
-    echo "No hardware acceleration available."
+    echo "No hardware acceleration available." >&2
 fi
