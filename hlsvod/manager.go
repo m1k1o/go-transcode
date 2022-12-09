256453c0cb26018acc36bfee81781470148564cd
Miroslav Šedivý <sedivy.miro@gmail.com>
2022-12-09 23:31:46 +0100

    add mutex to transcodeFromSegment. (#43)

--- hlsvod/manager.go
+++ hlsvod/manager.go
@@ -25,6 +25,7 @@ const readyTimeout = 80 * time.Second
 const transcodeTimeout = 10 * time.Second
 
 type ManagerCtx struct {
+	mu     sync.Mutex
 	logger zerolog.Logger
 	config Config
 
@@ -454,6 +455,9 @@ func (m *ManagerCtx) transcodeSegments(offset, limit int) error {
 }
 
 func (m *ManagerCtx) transcodeFromSegment(index int) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	segmentsTotal := len(m.segments)
 	if segmentsTotal <= m.segmentBufferMax {
 		// if all our segments can fit in the buffer
